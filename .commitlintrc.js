97e77624c9df0f0705c7e5245c8d657948adf5e9
Dancko C Manzor <dconcha@acid.cl>
2020-05-12 01:17:37 -0400

    FEATURE: hola

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "feature"]],
+    "type-enum": [2, "always", ["feat", "fix", "FEATURE"]],
+    "type-case": [0, "always", ["lower-case"]],
   },
 };
--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("prueba husky");
 console.log("prueba husky 2");
 
 console.log("prueba husky 3");
+
+console.log("prueba husky 4");
